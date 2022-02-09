f798d9fb291292bbc461a645e7e6972d1bd75e99
Aditya <mr.adityakaushal@gmail.com>
2022-02-09 17:40:17 +0530

    Added option to view profile of friends and their stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,36 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = @friends.without(current_user)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a friend name or email to search'
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 end
